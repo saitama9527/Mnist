7fb1ff764f6b1ed154e13760d05ab619649c171f
Saitama <taf10563@icloud.com>
2018-07-07 01:36:41 +0800

    mod

--- mnist.py
+++ mnist.py
@@ -20,6 +20,20 @@ print(y_train)
 y_test = np_utils.to_categorical(y_test, num_classes=10)
 print(y_test)
 
+# Another way to build your CNN
+model = Sequential()
+
+# Conv layer 1 output shape (32, 28, 28)
+model.add(Convolution2D(
+                        batch_input_shape=(None, 1, 28, 28),
+                        filters=32,
+                        kernel_size=5,
+                        strides=1,
+                        padding='same',     # Padding method
+                        data_format='channels_first',
+                        ))
+model.add(Activation('relu'))
+
 # Pooling layer 1 (max pooling) output shape (32, 14, 14)
 model.add(MaxPooling2D(
                        pool_size=2,
@@ -31,6 +45,15 @@ model.add(MaxPooling2D(
 # Conv layer 2 output shape (64, 14, 14)
 model.add(Convolution2D(64, 5, strides=1, padding='same', data_format='channels_first'))
 model.add(Activation('relu'))
+
+# Pooling layer 2 (max pooling) output shape (64, 7, 7)
+model.add(MaxPooling2D(2, 2, 'same', data_format='channels_first'))
+
+# Fully connected layer 1 input shape (64 * 7 * 7) = (3136), output shape (1024)
+model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation('relu'))
+
 
 
 
