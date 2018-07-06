b4b64a706293de3ec9c78103504d90f4ad30f579
Saitama <taf10563@icloud.com>
2018-07-07 01:37:09 +0800

    text

--- mnist.py
+++ mnist.py
@@ -10,50 +10,29 @@ import os
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# data pre-processing
-X_train = X_train.reshape(-1, 1,28, 28)/255.
-print(X_train)
-X_test = X_test.reshape(-1, 1,28, 28)/255.
-print(X_test)
-y_train = np_utils.to_categorical(y_train, num_classes=10)
-print(y_train)
-y_test = np_utils.to_categorical(y_test, num_classes=10)
-print(y_test)
 
-# Another way to build your CNN
-model = Sequential()
+# Another way to define your optimizer
+adam = Adam(lr=1e-4)
 
-# Conv layer 1 output shape (32, 28, 28)
-model.add(Convolution2D(
-                        batch_input_shape=(None, 1, 28, 28),
-                        filters=32,
-                        kernel_size=5,
-                        strides=1,
-                        padding='same',     # Padding method
-                        data_format='channels_first',
-                        ))
-model.add(Activation('relu'))
+# show model
 
-# Pooling layer 1 (max pooling) output shape (32, 14, 14)
-model.add(MaxPooling2D(
-                       pool_size=2,
-                       strides=2,
-                       padding='same',    # Padding method
-                       data_format='channels_first',
-                       ))
+model.summary()
 
-# Conv layer 2 output shape (64, 14, 14)
-model.add(Convolution2D(64, 5, strides=1, padding='same', data_format='channels_first'))
-model.add(Activation('relu'))
+# We add metrics to get more results you want to see
+model.compile(optimizer=adam,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-# Pooling layer 2 (max pooling) output shape (64, 7, 7)
-model.add(MaxPooling2D(2, 2, 'same', data_format='channels_first'))
+print('Training ------------')
+# Another way to train the model
+model.fit(X_train, y_train, epochs=1, batch_size=64)
 
-# Fully connected layer 1 input shape (64 * 7 * 7) = (3136), output shape (1024)
-model.add(Flatten())
-model.add(Dense(1024))
-model.add(Activation('relu'))
+print('\nTesting ------------')
+# Evaluate the model with the metrics we defined earlier
+loss, accuracy = model.evaluate(X_test, y_test)
 
+print('\ntest loss: ', loss)
+print('\ntest accuracy: ', accuracy)
 
 
 
