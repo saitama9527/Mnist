45416a1fe015c2c21985872c587e81dee5a3f48f
Saitama <taf10563@icloud.com>
2018-07-07 01:37:20 +0800

    train

--- mnist.py
+++ mnist.py
@@ -10,6 +10,53 @@ import os
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+# data pre-processing
+X_train = X_train.reshape(-1, 1,28, 28)/255.
+print(X_train)
+X_test = X_test.reshape(-1, 1,28, 28)/255.
+print(X_test)
+y_train = np_utils.to_categorical(y_train, num_classes=10)
+print(y_train)
+y_test = np_utils.to_categorical(y_test, num_classes=10)
+print(y_test)
+
+# Another way to build your CNN
+model = Sequential()
+
+# Conv layer 1 output shape (32, 28, 28)
+model.add(Convolution2D(
+                        batch_input_shape=(None, 1, 28, 28),
+                        filters=32,
+                        kernel_size=5,
+                        strides=1,
+                        padding='same',     # Padding method
+                        data_format='channels_first',
+                        ))
+model.add(Activation('relu'))
+
+# Pooling layer 1 (max pooling) output shape (32, 14, 14)
+model.add(MaxPooling2D(
+                       pool_size=2,
+                       strides=2,
+                       padding='same',    # Padding method
+                       data_format='channels_first',
+                       ))
+
+# Conv layer 2 output shape (64, 14, 14)
+model.add(Convolution2D(64, 5, strides=1, padding='same', data_format='channels_first'))
+model.add(Activation('relu'))
+
+# Pooling layer 2 (max pooling) output shape (64, 7, 7)
+model.add(MaxPooling2D(2, 2, 'same', data_format='channels_first'))
+
+# Fully connected layer 1 input shape (64 * 7 * 7) = (3136), output shape (1024)
+model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation('relu'))
+
+# Fully connected layer 2 to shape (10) for 10 classes
+model.add(Dense(10))
+model.add(Activation('softmax'))
 
 # Another way to define your optimizer
 adam = Adam(lr=1e-4)
@@ -33,7 +80,6 @@ loss, accuracy = model.evaluate(X_test, y_test)
 
 print('\ntest loss: ', loss)
 print('\ntest accuracy: ', accuracy)
-
 
 
 
